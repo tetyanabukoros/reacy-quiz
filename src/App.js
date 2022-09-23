2f54573be6c6e8047b5fec40e5471344447c64a2
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-09-23 14:12:11 +0300

    button try again fixed

--- src/App.js
+++ src/App.js
@@ -29,9 +29,7 @@ function Result({ correct }) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="Icon" />
       <h2>Правильных ответов: {correct} из {questions.length}</h2>
-      <a href="/">
-        <button>Попробовать снова</button>
-      </a>
+        <button onClick={() => window.location.reload()}>Попробовать снова</button>
     </div>
   );
 }
